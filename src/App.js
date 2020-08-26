79d89088c86b586727a9340d9b67fd706240ca84
VkAjay <59680173+VkAjay@users.noreply.github.com>
2020-08-26 19:49:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,135 @@
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 500
+      }
+    }
+  }
+}
+
+const initialState = {
+  input: '',
+  imageURL: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    doj: ''
+  }
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        doj: data.doj
+    }})
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onPictureSubmit = () => {
+    this.setState({imageURL: this.state.input})
+      fetch('http://localhost:3001/imageUrl', {
+        method: 'post',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        if(response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, {entries: count}));
+          })
+          .catch(console.log);
+        }
+        return this.calculateFaceLocation(response)
+      })
+      .then(faceloc => this.setState({box: faceloc}))
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange =(route) => {
+    if(route === 'signout') {
+      this.setState(initialState);
+    } else if(route === 'home') {
+      this.setState({isSignedIn: true});
+    }
+    this.setState({route: route})
+  }
+
+  render() {
+    const { isSignedIn, box, route, imageURL } =  this.state;
+    return (
+      <div className="App">
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === 'home' 
+        ? <div>
+            <Logo />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
+            <ImageLinkForm onInputChange={this.onInputChange} onPictureSubmit={this.onPictureSubmit} />
+            <FaceRecognition box={box} imageURL={imageURL} />
+          </div>
+        : (route === 'register'
+          ? <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          : <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          )
+        }
+      </div> 
+    );
+  }
+}
+
+export default App;
